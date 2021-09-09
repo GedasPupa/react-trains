b9f98b7319e0d89f85f6e08d88448af2898751f1
GedasPupa <gedosas@gmail.com>
2021-09-09 13:25:32 +0300

    react-trains

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import trains from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = ['Rudasis', 'Juodasis', 'Brigada1'];
 
-export default App;
+function App(props) {
+    return (
+        <>
+        {trains.map((train) => 
+            (<div>
+            <img src={'/' + train.img} />
+            <p>{train.desc}</p>
+            </div>)
+        )}
+        {/* <Kvadratas bebras="Juodasis" />
+        <Kvadratas bebras="Rudasis" />
+        <Kvadratas bebras="Brigadininkas" /> */}
+        </>
+
+    );
+} 
+
+export default App ;
+
+// namu darbas: github.com/A-Kija/vilkas 
+// nuotrukas nupiesti su title. turi tureti responsive design
